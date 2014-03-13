0f01c2afa8c146257fe0c6cb5f67bcc9e93e595b
srobledo <sergiorobledo2k5@gmail.com>
2014-03-13 11:15:55 -0500

    update form

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,8 @@ angular.module('nodeserverApp', [
   'ngResource',
   'ngSanitize',
   'ngRoute',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ng-grid'
 ])
   .config(function ($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
--- app/scripts/controllers/dashboard/address.js
+++ app/scripts/controllers/dashboard/address.js
@@ -2,12 +2,30 @@
 
 angular.module('nodeserverApp')
   .controller('AddressCtrl', function ($scope, $location,$routeParams, Auth) {
-
         $scope.login = function(form) {
+            $scope.submitted = true;
+
             console.log(form);
+
             if(form.$valid) {
                 console.log($scope.user);
             }
+            else{
+                var required = '';
+                for (var i = 0 ; i < form.$error.required.length; i++)
+                {
+                    required += form.$error.required[i].$name + ' ';
+                }
+                $scope.alerts = [
+                    { type: 'danger', msg: 'Oh snap! looks like '+required+' are missing.' }
+                ];
+                $scope.error = 'has-error';
+            }
+        };
+
+        $scope.closeAlert = function(index) {
+            $scope.alerts.splice(index, 1);
+            $scope.error = null;
         };
 
   });
