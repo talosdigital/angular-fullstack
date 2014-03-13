3d8cf2654090d20e6c1d88e4560523804b879dc6
srobledo <sergiorobledo2k5@gmail.com>
2014-03-13 12:05:40 -0500

    date

--- app/scripts/controllers/dashboard/address.js
+++ app/scripts/controllers/dashboard/address.js
@@ -5,8 +5,6 @@ angular.module('nodeserverApp')
         $scope.login = function(form) {
             $scope.submitted = true;
 
-            console.log(form);
-
             if(form.$valid) {
                 console.log($scope.user);
             }
@@ -26,6 +24,21 @@ angular.module('nodeserverApp')
         $scope.closeAlert = function(index) {
             $scope.alerts.splice(index, 1);
             $scope.error = null;
+        };
+
+        $scope.dateOptions = {
+            'year-format': "'yy'",
+            'starting-day': 1
+        };
+
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
+        $scope.format = $scope.formats[0];
+
+        $scope.open = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.opened = true;
         };
 
   });
