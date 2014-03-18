d1ded6c916d4bb7bdbed5d6c01a1be6d898296f9
srobledo <sergiorobledo2k5@gmail.com>
2014-03-18 13:52:23 -0500

    ui-router use

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,43 +5,66 @@ angular.module('nodeserverApp', [
   'ngResource',
   'ngSanitize',
   'ngRoute',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ui.router'
 ])
-  .config(function ($routeProvider, $locationProvider, $httpProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/main',
-        controller: 'MainCtrl'
-      })
-      .when('/dashboard', {
-        templateUrl: 'partials/dashboard/main',
-        controller: 'DashboardCtrl'
-      })
-        .when('/dashboard/:url', {
+  .config(function ($routeProvider, $locationProvider, $httpProvider , $stateProvider , $urlRouterProvider) {
+
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'partials/main',
+            controller: 'MainCtrl'
+        })
+        .state('dashboard', {
+            url: '/dashboard',
             templateUrl: 'partials/dashboard/main',
             controller: 'DashboardCtrl'
         })
-        .when('/dashboard/:url/:sub', {
-            templateUrl: 'partials/dashboard/main',
-            controller: 'DashboardCtrl'
+        .state('dashboard.welcome', {
+            url: '/welcome',
+            templateUrl: 'partials/dashboard/welcome'
         })
-      .when('/login', {
-        templateUrl: 'partials/login',
-        controller: 'LoginCtrl'
-      })
-      .when('/signup', {
-        templateUrl: 'partials/signup',
-        controller: 'SignupCtrl'
-      })
-      .when('/settings', {
-        templateUrl: 'partials/settings',
-        controller: 'SettingsCtrl',
-        authenticate: true
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-      
+        .state('dashboard.account', {
+            url: '/account',
+            templateUrl: 'partials/dashboard/account',
+            controller: 'AccountCtrl'
+        })
+        .state('dashboard.address', {
+            url: '/address',
+            templateUrl: 'partials/dashboard/address/index'
+
+        })
+        .state('dashboard.address.view', {
+            url: '/view',
+            templateUrl: 'partials/dashboard/address/view',
+            controller: 'AddressViewCtrl'
+
+        })
+        .state('dashboard.address.new', {
+            url: '/new',
+            templateUrl: 'partials/dashboard/address/new',
+            controller: 'AddressCtrl'
+
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: 'partials/login',
+            controller: 'LoginCtrl'
+        })
+        .state('signup', {
+            url: '/signup',
+            templateUrl: 'partials/signup',
+            controller: 'SignupCtrl'
+        })
+        .state('settings', {
+            url: '/settings',
+            templateUrl: 'partials/settings',
+            controller: 'SettingsCtrl',
+            authenticate: true
+        });
+    $urlRouterProvider.otherwise("/");
+
     $locationProvider.html5Mode(true);
       
     // Intercept 401s and 403s and redirect you to login
@@ -59,13 +82,12 @@ angular.module('nodeserverApp', [
       };
     }]);
   })
-  .run(function ($rootScope, $location, Auth) {
-
-    // Redirect to login if route requires auth and you're not logged in
-    $rootScope.$on('$routeChangeStart', function (event, next) {
-      
-      if (next.authenticate && !Auth.isLoggedIn()) {
-        $location.path('/login');
-      }
+    .run(function ($rootScope, $state, Auth) {
+        $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams){
+            if (toState.authenticate && !Auth.isLoggedIn()){
+                // User isn’t authenticated
+                $state.transitionTo("login");
+                event.preventDefault();
+            }
+        });
     });
-  });
--- app/scripts/controllers/dashboard/dashboard.js
+++ app/scripts/controllers/dashboard/dashboard.js
@@ -3,31 +3,4 @@
 angular.module('nodeserverApp')
   .controller('DashboardCtrl', function ($scope, $location,$routeParams, Auth) {
 
-        if($routeParams.sub){
-            switch($routeParams.sub)
-            {
-                case 'index':
-                    $scope.templateUrl = 'partials/dashboard/addressview';
-                    break;
-                case 'new':
-                    $scope.templateUrl = 'partials/dashboard/address';
-                    break;
-                default:
-                    $scope.templateUrl = 'partials/dashboard/welcome';
-            }
-        }
-        else{
-            switch($routeParams.url)
-            {
-                case 'main':
-                    $scope.templateUrl = 'partials/dashboard/welcome';
-                    break;
-                case 'account':
-                    $scope.templateUrl = 'partials/dashboard/account';
-                    break;
-                default:
-                    $scope.templateUrl = 'partials/dashboard/welcome';
-            }
-        }
-
   });
